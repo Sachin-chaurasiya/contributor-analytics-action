fb49c19c41798149d7879edc0e18f3758d40f0fb
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-05-20 22:56:19 +0530

    new build

--- dist/index.js
+++ dist/index.js
@@ -8058,16 +8058,18 @@ async function run() {
     const owner = repo.owner.login;
     const repoName = repo.name;
 
-    // Fetch contributors
-    const contributors = await octokit.repos.listContributors({
-      owner,
-      repo: repoName,
-    });
+    // // Fetch contributors
+    // const contributors = await octokit.repos.listContributors({
+    //   owner,
+    //   repo: repoName,
+    // });
 
-    console.log('Contributors:');
-    contributors.data.forEach((contributor) => {
-      console.log(`${contributor.login}: ${contributor.contributions}`);
-    });
+    // console.log('Contributors:');
+    // contributors.data.forEach((contributor) => {
+    //   console.log(`${contributor.login}: ${contributor.contributions}`);
+    // });
+
+    console.log({ octokit, owner, repoName });
   } catch (error) {
     console.error(error);
     process.exit(1);
